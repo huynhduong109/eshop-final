6d915ea841fcaf5398ecd638052140b26823559b
Huynh Van Duong <75488656+huynhduong109@users.noreply.github.com>
2023-07-31 22:22:25 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://eshop-final.vercel.app/',
     credentials: true
 }));
 
