681ae8bef61f108d92a4d7a025c2b3a903cf4984
Huynh Van Duong <75488656+huynhduong109@users.noreply.github.com>
2023-07-31 23:24:58 +0700

    Cloud

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const User = require("../model/user");
 const router = express.Router();
-const { upload } = require("../multer");
+const cloudinary = require("cloudinary");
 const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const fs = require("fs");
@@ -11,31 +11,27 @@ const sendMail = require("../utils/sendMail");
 const sendToken = require("../utils/jwtToken");
 const { isAuthenticated, isAdmin } = require("../middleware/auth");
 
-router.post("/create-user", upload.single("file"), async(req, res, next) => {
+router.post("/create-user", async (req, res, next) => {
     try {
-        const { name, email, password } = req.body;
+        const { name, email, password, avatar } = req.body;
         const userEmail = await User.findOne({ email });
 
         if (userEmail) {
-            const filename = req.file.filename;
-            const filePath = `uploads/${filename}`;
-            fs.unlink(filePath, (err) => {
-                if (err) {
-                    console.log(err);
-                    res.status(500).json({ message: "Error deleting file" });
-                }
-            });
             return next(new ErrorHandler("Người dùng đã tồn tại", 400));
         }
 
-        const filename = req.file.filename;
-        const fileUrl = path.join(filename);
+        const myCloud = await cloudinary.v2.uploader.upload(avatar, {
+      folder: "avatars",
+    });
 
         const user = {
             name: name,
             email: email,
             password: password,
-            avatar: fileUrl,
+            avatar: {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      },
         };
 
         const activationToken = createActivationToken(user);
@@ -218,24 +214,30 @@ router.put(
 router.put(
     "/update-avatar",
     isAuthenticated,
-    upload.single("image"),
     catchAsyncErrors(async(req, res, next) => {
         try {
-            const existsUser = await User.findById(req.user.id);
+            let existsUser = await User.findById(req.user.id);
+      if (req.body.avatar !== "") {
+        const imageId = existsUser.avatar.public_id;
 
-            const existAvatarPath = `uploads/${existsUser.avatar}`;
+            await cloudinary.v2.uploader.destroy(imageId);
 
-            fs.unlinkSync(existAvatarPath);
+            const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+          folder: "avatars",
+          width: 150,
+        });
 
-            const fileUrl = path.join(req.file.filename);
 
-            const user = await User.findByIdAndUpdate(req.user.id, {
-                avatar: fileUrl,
-            });
+            existsUser.avatar = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+            await existsUser.save();
 
             res.status(200).json({
                 success: true,
-                user,
+                user: existsUser,
             });
         } catch (error) {
             return next(new ErrorHandler(error.message, 500));
@@ -292,7 +294,6 @@ router.delete(
             const userId = req.user._id;
             const addressId = req.params.id;
 
-            console.log(addressId);
 
             await User.updateOne({
                 _id: userId,
@@ -393,6 +394,10 @@ router.delete(
                     new ErrorHandler("Người dùng không khả dụng với id này", 400)
                 );
             }
+
+            const imageId = user.avatar.public_id;
+
+      await cloudinary.v2.uploader.destroy(imageId);
 
             await User.findByIdAndDelete(req.params.id);
 
